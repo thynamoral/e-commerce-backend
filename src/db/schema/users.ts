be30482c99dd532b9e67dc407c90a0680843ac20
Namoral Thy <thynamoral18@gmail.com>
2025-03-17 02:03:56 +0700

    [update] user schema

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -9,7 +9,7 @@ import {
 export const usersTable = pgTable("USERS", {
   id: uuid().defaultRandom().primaryKey(),
   email: varchar({ length: 255 }).notNull().unique(),
-  pasword: varchar({ length: 255 }),
+  password: varchar({ length: 255 }),
   firstName: varchar({ length: 100 }),
   lastName: varchar({ length: 100 }),
   role: varchar({ length: 50 }).notNull().default("user"),
@@ -17,6 +17,7 @@ export const usersTable = pgTable("USERS", {
   isEmailVerified: boolean().default(false),
   verificationToken: varchar({ length: 255 }),
   resetPasswordToken: varchar({ length: 255 }),
+  resetPasswordExpires: timestamp(),
   createdAt: timestamp().defaultNow(),
   updatedAt: timestamp().defaultNow(),
 });
