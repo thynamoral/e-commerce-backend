0e6ded82c10d0a0fb5c700f76175b5d0c6420112
Namoral Thy <thynamoral18@gmail.com>
2025-03-17 14:06:17 +0700

    [add] async middlware handler to handle router handler

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -107,3 +107,12 @@ export const authErrorHandler = (
 
   next(err);
 };
+
+/**
+ * Async handler middleware wrapper
+ */
+export const asyncHandler =
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
