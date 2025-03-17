82a0139860330402b88538ba096859ca007d37d9
Namoral Thy <thynamoral18@gmail.com>
2025-03-17 14:06:34 +0700

    [delete] seed script

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,16 +6,16 @@ loadEnvVariables();
 
 export const db = drizzle(process.env.DATABASE_URL!);
 
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    email: "testing@gmail.com",
-  };
+// async function main() {
+//   const user: typeof usersTable.$inferInsert = {
+//     email: "testing@gmail.com",
+//   };
 
-  await db.insert(usersTable).values(user);
-  console.log("Create new user!");
+//   await db.insert(usersTable).values(user);
+//   console.log("Create new user!");
 
-  const users = await db.select().from(usersTable);
-  console.log("All users", users);
-}
+//   const users = await db.select().from(usersTable);
+//   console.log("All users", users);
+// }
 
-main();
+// main();
