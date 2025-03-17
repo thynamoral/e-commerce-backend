5aaac44ad0c7a27d5333a4e81999c2f6cf08dadc
Namoral Thy <thynamoral18@gmail.com>
2025-03-17 18:02:08 +0700

    [update] application entry logics

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { loadEnvVariables } from "./configs/env";
 import express, { Request, Response } from "express";
+import authRouter from "./resources/auth/authRoute";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import { configurePassport } from "./configs/passport";
 
 loadEnvVariables();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json("Hello world!");
-});
+configurePassport();
+
+// auth routes
+app.use("/auth", authRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running at ${process.env.API_URL}!`);
