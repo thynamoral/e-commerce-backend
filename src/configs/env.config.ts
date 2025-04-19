734acdd48a4a5c0c5514768514d4baaa8b20fae4
Namoral Thy <thynamoral18@gmail.com>
2025-04-19 15:12:32 +0700

    [init] commit

---
+++ src/configs/env.config.ts
@@ -0,0 +1,10 @@
+const getENV = (key: string, defaultValue?: string) => {
+  const value = process.env[key] || defaultValue;
+  if (value === undefined) {
+    throw new Error(`Environment variable ${key} is missing`);
+  }
+
+  return value;
+};
+
+export const PORT = getENV("PORT");
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import cookieParser from "cookie-parser";
+import { PORT } from "./configs/env.config";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
