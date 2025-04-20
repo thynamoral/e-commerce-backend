0fbf2969ee45850d87b55a401f94799dc0f52a28
Namoral Thy <thynamoral18@gmail.com>
2025-04-20 17:34:00 +0700

    [update] entities

--- src/entities/VerificationCode.entity.ts
+++ src/entities/VerificationCode.entity.ts
@@ -2,6 +2,6 @@ export type VerificationCode = {
   verification_code_id: string;
   user_id: string;
   type: string;
-  createdAt: Date;
-  updatedAt: Date;
+  createdat: Date;
+  expiredat: Date;
 };
