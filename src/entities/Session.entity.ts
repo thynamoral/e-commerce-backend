60b9a2f9096e79309150712c7ce41816574f1771
Namoral Thy <thynamoral18@gmail.com>
2025-04-20 14:42:42 +0700

    [add] entities

---
+++ src/entities/Session.entity.ts
@@ -0,0 +1,6 @@
+export type Session = {
+  session_id: string;
+  user_id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
---
+++ src/entities/VerificationCode.entity.ts
@@ -0,0 +1,7 @@
+export type VerificationCode = {
+  verification_code_id: string;
+  user_id: string;
+  type: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
