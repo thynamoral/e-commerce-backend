cacfbeb55369d22d94f4d042d9df7a5b7da671e7
Namoral Thy <thynamoral18@gmail.com>
2025-04-21 14:05:44 +0700

    [update] utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,3 +3,7 @@ export const fifteenMinutesFromNow = () =>
 
 export const sevenDaysFromNow = () =>
   new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+export const tenMinutesAgo = () => new Date(Date.now() - 10 * 60 * 1000);
+
+export const convertToMs = (date: Date) => date.getTime();
