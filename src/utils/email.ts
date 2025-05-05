d44a4b40d47023495660373cde280e624415815e
Namoral Thy <thynamoral18@gmail.com>
2025-05-05 17:22:02 +0700

    [update] sending email logics

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,27 +1,34 @@
-import { Resend } from "resend";
-import { EMAIL_SENDER, NODE_ENV, RESEND_API_KEY } from "../configs/env.config";
+import nodemailer from "nodemailer";
+import { APP_PASSWORD, EMAIL_SENDER } from "../configs/env.config";
 
-export const resend = new Resend(RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL_SENDER,
+    pass: APP_PASSWORD,
+  },
+});
 
-type ResendParams = {
+type SendEmailParams = {
   to: string;
   subject: string;
   html: string;
   text: string;
 };
 
-export const getEmailFrom = () => "onboarding@resend.dev";
-
-export const getEmailTo = (to: string) =>
-  NODE_ENV === "production" ? to : "delivered@resend.dev";
-
-export const sendEmail = async ({ to, subject, html, text }: ResendParams) => {
-  return await resend.emails.send({
-    from: getEmailFrom(),
-    to: getEmailTo(to),
+export const sendEmail = async ({
+  to,
+  subject,
+  html,
+  text,
+}: SendEmailParams) => {
+  return await transporter.sendMail({
+    from: `"Momo E-Commerce" <${EMAIL_SENDER}>`,
+    to,
     subject,
     html,
     text,
+    replyTo: EMAIL_SENDER,
   });
 };
 
@@ -29,11 +36,69 @@ export const sendEmail = async ({ to, subject, html, text }: ResendParams) => {
 export const getVerifyEmailTemplate = (url: string) => ({
   subject: "Verify Email Address",
   text: `Click on the link to verify your email address: ${url}`,
-  html: `<!doctype html><html lang="en-US"><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"/><title>Verify Email Address Email Template</title><meta name="description" content="Verify Email Address Email Template."><style type="text/css">a:hover{text-decoration:underline!important}</style></head><body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0"><!--100%body table--><table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;"><tr><td><table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0"><tr><td style="height:80px;">&nbsp;</td></tr><tr><td style="text-align:center;"></a></td></tr><tr><td style="height:20px;">&nbsp;</td></tr><tr><td><table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);"><tr><td style="height:40px;">&nbsp;</td></tr><tr><td style="padding:0 35px;"><h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Please verify your email address</h1><span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span><p style="color:#455056; font-size:15px;line-height:24px; margin:0;">Click on the following link to verify your email address.</p><a target="_blank" href="${url}" style="background:#2f89ff;text-decoration:none !important; font-weight:500; margin-top:24px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Verify Email Address</a></td></tr><tr><td style="height:40px;">&nbsp;</td></tr></table></td><tr><td style="height:20px;">&nbsp;</td></tr><tr><td style="text-align:center;"><p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; Momo E-Commerce</p></td></tr><tr><td style="height:80px;">&nbsp;</td></tr></table></td></tr></table><!--/100%body table--></body></html>`,
+  html: `<!doctype html><html lang="en-US">
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <title>Verify Email Address</title>
+    <meta name="description" content="Verify your email.">
+    <style type="text/css">a:hover{text-decoration:underline!important}</style>
+  </head>
+  <body style="margin:0; background-color:#f2f3f8;">
+    <table width="100%" bgcolor="#f2f3f8" style="font-family: 'Open Sans', sans-serif;">
+      <tr><td>
+        <table style="max-width:670px; margin:0 auto; background:#fff; border-radius:3px; text-align:center; box-shadow:0 6px 18px rgba(0,0,0,.06);" width="100%">
+          <tr><td style="height:40px;">&nbsp;</td></tr>
+          <tr><td style="padding:0 35px;">
+            <h1 style="color:#1e1e2d; font-weight:500; font-size:32px;">Please verify your email address</h1>
+            <span style="display:inline-block; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+            <p style="color:#455056; font-size:15px;">Click the link below to verify your email.</p>
+            <a href="${url}" target="_blank" style="background:#2f89ff; color:#fff; padding:10px 24px; border-radius:50px; text-transform:uppercase; text-decoration:none;">Verify Email Address</a>
+          </td></tr>
+          <tr><td style="height:40px;">&nbsp;</td></tr>
+        </table>
+        <table width="100%" style="max-width:670px; margin:20px auto; text-align:center;">
+          <tr>
+            <td style="font-size:14px; color:#455056;">
+              &copy; Momo E-Commerce
+            </td>
+          </tr>
+        </table>
+      </td></tr>
+    </table>
+  </body></html>`,
 });
 
 export const getPasswordResetTemplate = (url: string) => ({
   subject: "Password Reset Request",
   text: `You requested a password reset. Click on the link to reset your password: ${url}`,
-  html: `<!doctype html><html lang="en-US"><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"/><title>Reset Password Email Template</title><meta name="description" content="Reset Password Email Template."><style type="text/css">a:hover{text-decoration:underline!important}</style></head><body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0"><!--100%body table--><table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;"><tr><td><table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0"><tr><td style="height:80px;">&nbsp;</td></tr><tr><td style="text-align:center;"></a></td></tr><tr><td style="height:20px;">&nbsp;</td></tr><tr><td><table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);"><tr><td style="height:40px;">&nbsp;</td></tr><tr><td style="padding:0 35px;"><h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have requested to reset your password</h1><span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span><p style="color:#455056; font-size:15px;line-height:24px; margin:0;">A unique link to reset your password has been generated for you. To reset your password, click the following link and follow the instructions.</p><a target="_blank" href="${url}" style="background:#2f89ff;text-decoration:none !important; font-weight:500; margin-top:24px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset Password</a></td></tr><tr><td style="height:40px;">&nbsp;</td></tr></table></td><tr><td style="height:20px;">&nbsp;</td></tr><tr><td style="text-align:center;"><p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; Momo E-Commerce</p></td></tr><tr><td style="height:80px;">&nbsp;</td></tr></table></td></tr></table><!--/100%body table--></body></html>`,
+  html: `<!doctype html><html lang="en-US">
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <title>Password Reset</title>
+    <meta name="description" content="Reset your password.">
+    <style type="text/css">a:hover{text-decoration:underline!important}</style>
+  </head>
+  <body style="margin:0; background-color:#f2f3f8;">
+    <table width="100%" bgcolor="#f2f3f8" style="font-family: 'Open Sans', sans-serif;">
+      <tr><td>
+        <table style="max-width:670px; margin:0 auto; background:#fff; border-radius:3px; text-align:center; box-shadow:0 6px 18px rgba(0,0,0,.06);" width="100%">
+          <tr><td style="height:40px;">&nbsp;</td></tr>
+          <tr><td style="padding:0 35px;">
+            <h1 style="color:#1e1e2d; font-weight:500; font-size:32px;">You requested to reset your password</h1>
+            <span style="display:inline-block; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+            <p style="color:#455056; font-size:15px;">Click the link below to reset your password.</p>
+            <a href="${url}" target="_blank" style="background:#2f89ff; color:#fff; padding:10px 24px; border-radius:50px; text-transform:uppercase; text-decoration:none;">Reset Password</a>
+          </td></tr>
+          <tr><td style="height:40px;">&nbsp;</td></tr>
+        </table>
+        <table width="100%" style="max-width:670px; margin:20px auto; text-align:center;">
+          <tr>
+            <td style="font-size:14px; color:#455056;">
+              &copy; Momo E-Commerce
+            </td>
+          </tr>
+        </table>
+      </td></tr>
+    </table>
+  </body></html>`,
 });
